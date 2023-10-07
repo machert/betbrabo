08437bf08eaad0d451002564fcbed72dc3b11cef
Felipe Machert <felipe.costa@vennx.com.br>
2023-10-07 11:33:10 -0300

    Reestruturando pastas web

--- betbrabo-web/betbrabo-web/src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './routes/index.js'
-
-// createApp(App).mount('#app')
-createApp(App).use(router).mount('#app')
--- betbrabo-web/betbrabo-web/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import http from './config'
-
-export const login = (email, password) => {
-    const params = {
-        'email' : email,
-        'password' : password,
-    }
-    return http.post('login', params)
-}
